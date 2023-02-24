fff5487cc4f5849a34a713967da31e9488bb11e8
kiprop-dave <davekip16@gmail.com>
2023-02-24 15:51:47 +0300

    select bug fixed

--- src/components/Modal/ModalSelect.tsx
+++ src/components/Modal/ModalSelect.tsx
@@ -10,13 +10,7 @@ interface SelectProps {
 
 function ModalSelect({ options, selected, setSelected }: SelectProps) {
   return (
-    // Bug: The value of the select element is not updated when the selected
-    // TODO:Read more about the select element
-    <select
-      className={S.select}
-      value={selected.name}
-      onChange={(e) => setSelected(e.target.value)}
-    >
+    <select className={S.select} onChange={(e) => setSelected(e.target.value)}>
       {options.map((option) => (
         <option key={option.id} value={option.id}>
           {option.name}
