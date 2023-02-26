a23663ff965fa3020cea0b6df22b3794f3b296fc
kiprop-dave <davekip16@gmail.com>
2023-02-26 18:03:56 +0300

    board colums desktop done

--- src/components/Kanban/Column.tsx
+++ src/components/Kanban/Column.tsx
@@ -1,20 +1,45 @@
 import S from "./Kanban.module.css";
+import useAppContext from "../../hooks/useAppContext";
+import { useEffect, useState } from "react";
 import generateRGB from "../../utils/generateRGB";
 import { Column } from "../../types/types";
+import BoardTask from "./Task";
 
 type ColumnProps = {
   column: Column;
 };
 
 const BoardColumn = ({ column }: ColumnProps): JSX.Element => {
-  const { name, tasks } = column;
+  const [iconBg, setIconBg] = useState<React.CSSProperties>({});
+  const { openModal, selectActiveTask } = useAppContext();
+  const { name, tasks, id } = column;
+
+  useEffect(() => {
+    setIconBg({ backgroundColor: generateRGB() });
+  }, []);
+
+  let col__empty = tasks.length === 0 ? S.col__empty : "";
+
+  const openTaskModal = (colId: string, taskId: string) => {
+    selectActiveTask(colId, taskId);
+    openModal("viewTask");
+  };
+
+  const taskElements = tasks.map((task) => (
+    <BoardTask key={task.id} action={openTaskModal} colId={id} task={task} />
+  ));
 
   return (
     <>
       <div className={`${S.columnContainer}`}>
         <div className={`${S.columnHeader}`}>
-          <span className={`${S.icon}`}></span>
-          <h3 className={`${S.columnTitle}`}>{name.toUpperCase()}</h3>
+          <span className={`${S.icon}`} style={iconBg}></span>
+          <h3 className={`${S.columnTitle}`}>{`${name.toUpperCase()} (${
+            tasks.length
+          })`}</h3>
+        </div>
+        <div className={`${S.tasksContainer} ${col__empty}`}>
+          {taskElements}
         </div>
       </div>
     </>
---
+++ src/components/Kanban/Task.tsx
@@ -0,0 +1,30 @@
+import S from "./Kanban.module.css";
+import useThemeContext from "../../hooks/useThemeContext";
+import { Task } from "../../types/types";
+
+type TaskProps = {
+  task: Task;
+  colId: string;
+  action: (colId: string, taskId: string) => void;
+};
+
+const BoardTask = ({ task }: TaskProps): JSX.Element => {
+  const { theme1 } = useThemeContext();
+  const { title, id, subTasks } = task;
+
+  const paragraphContent = (): string => {
+    let completed = subTasks.filter((subTask) => subTask.completed).length;
+    return `${completed} of ${subTasks.length} completed`;
+  };
+
+  return (
+    <>
+      <div className={`${S.task} ${theme1}`}>
+        <h4 className={`${S.taskTitle}`}>{title}</h4>
+        <p className={`${S.par} util`}>{paragraphContent()}</p>
+      </div>
+    </>
+  );
+};
+
+export default BoardTask;
